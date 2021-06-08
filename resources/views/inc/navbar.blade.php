51b1d960adea5197ce50dc7d2bce1b06fba1ea86
Konuzen <arjunaacchadipa@gmail.com>
2021-06-08 21:51:13 +0700

    navbar completed

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,14 +1,13 @@
 <nav class="navbar navbar-expand navbar-dark" style="background-color: #A67B5B" aria-label="Second navbar example">
     <div class="container">
-      <a class="navbar-brand" href="/">{{config("app.name", "Beans")}}</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample02" aria-controls="navbarsExample02" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample02" aria-controls="navbarsExample02" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-      <div class="collapse navbar-collapse" id="navbarsExample02">
+        <div class="collapse navbar-collapse" id="navbarsExample02">
             <ul class="navbar-nav me-auto">
                 <li class="nav-item">
-                    <a class="nav-link" style="font-size: 15px; color: white" aria-current="page" href="#">Home</a>
+                    <a class="nav-link" style="font-size: 15px; color: white" aria-current="page" href="/">Home</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" style="font-size: 15px; color: white" href="/about">About</a>
@@ -18,7 +17,11 @@
                 <input class="form-control me-2" style="font-size: 15px" type="search" placeholder="Search" aria-label="Search">
                 <button class="btn btn-outline-success" style="font-size: 15px; color: white; background-color: #282E40; border: #282E40" type="submit">Search</button>
             </form>
+            <a class="navbar-brand; top: 50%" style="position: absolute; left: 50%" href="/">
+                <img src="images/logo.png" alt="/" width="100" height="85"/>
+            </a>
         </div>
+        
         <div style="float: right">
             <form class="d-flex">
                 <button href="/login" class="btn btn-outline-success" style="font-size: 15px; color: white; background-color: #282E40; border: #282E40" type="submit">Login</button>
