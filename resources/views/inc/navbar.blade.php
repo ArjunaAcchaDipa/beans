10cd2e8aaeebba40af65676a8180c814cb50c6c7
vantasm <khoeric1@yahoo.co.id>
2021-06-09 15:51:42 +0700

    hapus link bootstrap di navbar

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,10 +1,3 @@
- <!-- Bootstrap CSS -->
-<link
-href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
-rel="stylesheet"
-integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
-crossorigin="anonymous"
-/>
 <nav class="navbar navbar-expand navbar-dark" style="background-color: #A67B5B; position: absolute; width: 100%; padding-top: 10px">
     <div class="container">
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" aria-controls="navbarsExample02" aria-expanded="false" aria-label="Toggle navigation">
