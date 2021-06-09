a7f3b4d5f02e37dd162db7b4f1b3edc351a1196b
vantasm <khoeric1@yahoo.co.id>
2021-06-09 16:36:32 +0700

    atur peletakan footer untuk register dan login

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -1,51 +1,54 @@
 <footer id="footer" style="background-color: #A67B5B; bottom: 0; position: relative; width: 100%">
-    <div class="container" style="padding-top: 20px">
-        <div class="row">
-            <div class="col-lg-3 col-md-6 col-sm-12" style="font-size: 15px; color: white">
-                <h3>Popular Coffee Shop</h3>
-                <div style="padding-top: 10px">
-                    <p>
-                        Norma Coffee<br>
-                        Monks<br>
-                        Makna Coffee
-                    </p>
+        <div class="position-absolute top-0 start-50 translate-middle"></div>
+            <div class="container" style="padding-top: 20px">
+                <div class="row">
+                    <div class="col-lg-3 col-md-6 col-sm-12" style="font-size: 15px; color: white">
+                        <h3>Popular Coffee Shop</h3>
+                        <div style="padding-top: 10px">
+                            <p>
+                                Norma Coffee<br>
+                                Monks<br>
+                                Makna Coffee
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 col-sm-12" style="font-size: 15px; color: white">
+                        <h3>Popular Search</h3>
+                        <div style="padding-top: 10px">
+                            <p>
+                                Café Batavia<br>
+                                Lucky Cat Coffee & Kitchen<br>
+                                The Garden
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 col-sm-12" style="font-size: 15px; color: white">
+                        <h3>Beans</h3>
+                        <div style="padding-top: 10px">
+                            <p>
+                                About Us<br>
+                                Help Center<br>
+                                Privacy Policy
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 col-sm-12" style="font-size: 15px; color: white">
+                        <h3>Follow Us</h3>
+                        <p>
+                            <span>
+                                <img src="images/fb_logo.png" width="38" height="38">
+                            </span>
+                            <span>
+                                <a href="https://instagram.com/beans_coffeemap" style="margin-left: 10px">
+                                    <img src="images/ig_logo.png" width="46" height="46">
+                                </a>
+                            </span>
+                            <span>
+                                <img src="images/twitter_logo.png" width="58" height="58">
+                            </span>
+                        </p>
+                    </div>
                 </div>
-            </div>
-            <div class="col-lg-3 col-md-6 col-sm-12" style="font-size: 15px; color: white">
-                <h3>Popular Search</h3>
-                <div style="padding-top: 10px">
-                    <p>
-                        Café Batavia<br>
-                        Lucky Cat Coffee & Kitchen<br>
-                        The Garden
-                    </p>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6 col-sm-12" style="font-size: 15px; color: white">
-                <h3>Beans</h3>
-                <div style="padding-top: 10px">
-                    <p>
-                        About Us<br>
-                        Help Center<br>
-                        Privacy Policy
-                    </p>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6 col-sm-12" style="font-size: 15px; color: white">
-                <h3>Follow Us</h3>
-                <p>
-                    <span>
-                        <img src="images/fb_logo.png" width="38" height="38">
-                    </span>
-                    <span>
-                        <a href="https://instagram.com/beans_coffeemap" style="margin-left: 10px">
-                            <img src="images/ig_logo.png" width="46" height="46">
-                        </a>
-                    </span>
-                    <span>
-                        <img src="images/twitter_logo.png" width="58" height="58">
-                    </span>
-                </p>
             </div>
         </div>
     </div>
