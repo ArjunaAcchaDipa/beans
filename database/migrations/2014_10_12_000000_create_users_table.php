da3af4d994c46bbdbf0c83082d99930fc69328f4
Konuzen <arjunaacchadipa@gmail.com>
2021-06-11 18:14:56 +0700

    fixing navbar (final) + add some table to migrate

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,9 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->integer('review')->unsigned()->default('0');
+            $table->integer('followers')->unsigned()->default('0');
+            $table->integer('following')->unsigned()->default('0');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,64 +1,53 @@
 <nav class="navbar navbar-expand navbar-dark" style="background-color: #A67B5B; top: 0; width: 100%; padding-top: 10px">
     <div class="container">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" aria-controls="navbarsExample02" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbar" style="padding-left: 5%">
+        <div class="collapse navbar-collapse container" id="navbar" style="padding-left: 5%">
             <ul class="navbar-nav me-auto">
-                <li class="nav-item">
+                <li class="nav-item container">
                     <a class="nav-link" style="font-size: 15px; color: white" aria-current="page" href="{{ url('/')}}">Home</a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item container">
                     <a class="nav-link" style="font-size: 15px; color: white" href="/about">About</a>
                 </li>
             </ul>
-            <form class="d-flex">
-                <input class="form-control me-2" style="font-size: 15px; position: relative; right: 19cm" type="search" placeholder="Coffee Shop" aria-label="Search">
-                <button class="btn btn-outline-success" style="font-size: 15px; color: white; background-color: #282E40; border: #282E40; position: relative;right: 19cm" type="submit">Search</button>
+            <form class="d-flex" style="padding-top: 6px; margin-bottom: 10px; margin-left: 200px; position: absolute">
+                <input class="form-control me-2" style="font-size: 15px; right: 18cm; width: 204px; height: 32px" type="search" placeholder="Coffee Shop" aria-label="Search">
+                <button class="btn btn-outline-success" style="font-size: 15px; color: white; background-color: #282E40; border: #282E40; right: 18cm" type="submit">Search</button>
             </form>
             <a class="navbar-brand; top: 50%" style="position: absolute; left: 50%" href="{{ url('/')}}">
-                <img src="images/logo.png" alt="Coffee Beans" width="100" height="85"/>
+                <img src="/images/logo.png" alt="Coffee Beans" width="100" height="85"/>
             </a>
         </div>
         
         <div style="float: left">
             <ul class="navbar-nav">
                 @if (Auth::guest())
-                <div style="float: right">
-                    <form class="d-flex">
-                       <a href="{{ route('login')}}" class="btn btn-outline-success" style="font-size: 15px; color: white; background-color: #282E40; border: #282E40" type="submit">Login</a>
-        
-                    </form>
-                </div>
-                <div style="float: right">
-                    <form class="d-flex">
-                        <div class="container">
+                    <div class="container" style="float: right">
+                        <form class="d-inline-flex">
+                            <a href="{{ route('login')}}" class="btn btn-outline-success" style="font-size: 15px; color: white; background-color: #282E40; border: #282E40" type="submit">Login</a>
                             <a href="{{ route('register')}}" class="btn btn-outline-success" style="font-size: 15px; color: white; background-color: #282E40; border: #282E40" type="submit">Register</a>
-                        </div>
-                    </form>
-                </div>
-                @else   
-                <div class="dropdown">
-                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="background-color:#282E40;font-size: 15px">
-                      Hello, {{ Auth::user()->name }}
-                    </button>
-                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                        <a  class="dropdown-item" href="{{ url('profile') }}" aria-current="page" style="font-size: 15px">
-                            Profile
-                        </a> 
-                          
-                        <a class="dropdown-item" href="{{ route('logout') }}"
-                        onclick="event.preventDefault();
-                        document.getElementById('logout-form').submit();"
-                        aria-current="page" style="font-size: 15px">
-                            Logout
-                        </a>
+                        </form>
                     </div>
-                </div>
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                            {{ csrf_field() }}
-                        </form>    
+                @else   
+                    <div class="dropdown container">
+                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="background-color:#282E40; font-size: 15px">
+                            Hello, {{ Auth::user()->name }}
+                        </button>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <a  class="dropdown-item" href="{{ url('profile') }}" aria-current="page" style="font-size: 15px">
+                                Profile
+                            </a> 
+                            
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                            onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();"
+                            aria-current="page" style="font-size: 15px">
+                                Logout
+                            </a>
+                        </div>
+                    </div>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        {{ csrf_field() }}
+                    </form>    
                 @endif
             </ul>
         </div>
