2a6894a56f7b5dd09a90b75d812f4f6b03910e90
vantasm <khoeric1@yahoo.co.id>
2021-06-13 16:26:24 +0700

    route /home ganti ke /

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ Route::get('/profile', function () {
 Auth::routes();
 
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/home', [App\Http\Controllers\PagesController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\PagesController::class, 'index'])->name('home');
 Route::get('/editprofile/{id}', [App\Http\Controllers\ProfileController::class, 'show'])->name('editprofile');
 Route::get('/coffeeshop/{id}', [App\Http\Controllers\ShopController::class, 'show'])->name('coffeeshop');
 Route::post('edit', [App\Http\Controllers\ProfileController::class, 'update'])->name('edit');
